b2b8b4d00a55606a3ab258510818f25b48e96e50
Nicholas Martini <nicho.martini@gmail.com>
2019-01-19 11:42:55 +0100

    Add project files.

---
+++ NoSleep/Config.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace NoSleep
+{
+    class Config
+    {
+        #region Properties
+
+        public bool enabled { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        //Write the JSON file to the disk (https://stackoverflow.com/questions/16921652/how-to-write-a-json-file-in-c)
+        public static void WriteConfig(Config config)
+        {
+            using (StreamWriter streamWriter = File.CreateText(Path.Combine(GetNoSleepPath(), @"config.json")))
+            {
+                JsonSerializer jsonSerializer = new JsonSerializer()
+                {
+                    Formatting = Formatting.Indented
+                };
+
+                jsonSerializer.Serialize(streamWriter, config);
+            }
+        }
+
+        //Read the JSON config file
+        public static Config ReadConfig()
+        {
+            string s = "";
+
+            using (StreamReader streamReader = new StreamReader(Path.Combine(GetNoSleepPath(), @"config.json")))
+            {
+                s = streamReader.ReadToEnd();
+            }
+
+            Config config = JsonConvert.DeserializeObject<Config>(s);
+
+            return config;
+        }
+
+        //Get the app folder path
+        private static string GetNoSleepPath()
+        {
+            //Get the AppData path
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            //Don't add the "\" at the beginning of the path2 or else it'll return path2 (https://stackoverflow.com/questions/18008276/why-the-path-combine-is-not-combining-the-path-and-file)
+            string noSleepPath = Path.Combine(appDataPath, @"NoSleep");
+
+            return noSleepPath;
+        }
+
+        //Create app folder and config file if not already existing
+        public static void FirstSetup()
+        {
+            if (!Directory.Exists(GetNoSleepPath()))
+            {
+                Directory.CreateDirectory(GetNoSleepPath());
+            }
+
+            if (!File.Exists(Path.Combine(GetNoSleepPath(), @"config.json")))
+            {
+                WriteConfig(new Config() { enabled = false });
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ NoSleep/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace NoSleep
+{
+    public partial class Form1 : Form
+    {
+        private static NotifyIcon notifyIcon = new NotifyIcon();
+        private static System.Timers.Timer timer = new System.Timers.Timer();
+        private static bool isEnabled = Config.ReadConfig().enabled;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Check if config file exists else create app folder and config file
+            try
+            {
+                Config.ReadConfig();
+            }
+            catch (Exception)
+            {
+                Config.FirstSetup();
+            }
+
+            //Start timer
+            SetTimer();
+
+            //Start the timer if enabled
+            if (isEnabled)
+            {
+                timer.Start();
+            }
+        }
+
+        #region Methods
+
+        //Set the timer to send the keystroke
+        private static void SetTimer()
+        {
+            //Attach the Elapsed event to the timer
+            timer.Elapsed += TimerElapsed;
+
+            //Set the timer at 2 seconds
+            timer.Interval = 2000;
+            timer.AutoReset = true;
+        }
+
+        //Create the ContextMenuStrip
+        private static ContextMenuStrip AddContextMenuStrip()
+        {
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip();
+            contextMenuStrip.Items.Add("Exit");
+            contextMenuStrip.ItemClicked += ContextMenuStrip_ItemClicked;
+
+            return contextMenuStrip;
+        }
+
+        //Check if app is enabled and set the correct icon
+        private static Icon AssignIcon()
+        {
+            Icon icon;
+
+            if (isEnabled)
+            {
+                icon = Properties.Resources.EnabledIcon;
+            }
+            else
+            {
+                icon = Properties.Resources.DisabledIcon;
+            }
+
+            return icon;
+        }
+
+        #endregion
+
+        #region Events
+
+        //Exit the application
+        private static void ContextMenuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Simulate the keystroke when the timer ticks
+        private static void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            //Send the keystoke (https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.sendkeys?redirectedfrom=MSDN&view=netframework-4.7.2)
+            SendKeys.SendWait("{F15}");
+        }
+
+        //Handle the Resize event in order to minimize to tray (https://www.codeproject.com/Articles/27599/Minimize-window-to-system-tray)
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                ShowInTaskbar = false;
+
+                notifyIcon.Visible = true;
+                notifyIcon.Icon = AssignIcon();
+                notifyIcon.ContextMenuStrip = AddContextMenuStrip();
+                notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+                this.Hide();
+            }
+        }
+
+        //Enable/disable app when double-clicking icon
+        private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            if (isEnabled)
+            {
+                Config.WriteConfig(new Config { enabled = false });
+                isEnabled = false;
+                notifyIcon.Icon = AssignIcon();
+
+                //Stop timer
+                timer.Stop();
+            }
+            else
+            {
+                Config.WriteConfig(new Config { enabled = true });
+                isEnabled = true;
+                notifyIcon.Icon = AssignIcon();
+
+                //Start timer
+                timer.Start();
+            }
+        }
+
+        //(Try to) Remove the NotifyIcon from the tray bar
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            notifyIcon.Icon.Dispose();
+            notifyIcon.Dispose();
+        }
+
+        //Minimize the windows on load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        #endregion
+    }
+}
